a29f0ba344aca41746716f5bb942c2f5e855a522
RDambreville <robert.dambreville@gmail.com>
2018-05-21 00:28:38 -0400

    long data types instead of ints; almost correct inversions calculation

--- src/Chapter2_Getting_Started/MergeSort.java
+++ src/Chapter2_Getting_Started/MergeSort.java
@@ -6,12 +6,12 @@ public class MergeSort {
 
     static int inversions = 0;
 
-    public static int[] Merge(int[] A, int start, int mid, int end){
+    public static long[] Merge(long[] A, int start, int mid, int end){
         int n1 = mid - start + 1;
         int n2 = end - mid;
 
-        int[] L = new int[n1+1];
-        int[] R = new int[n2+1];
+        long[] L = new long[n1+1];
+        long[] R = new long[n2+1];
 
         // populate the left deck
         for(int i = 0; i < L.length-1; i++){
@@ -37,11 +37,12 @@ public class MergeSort {
             }else /*if(R[j] != -1)*/{
                 A[k] = R[j];
                 if(L[i] != Integer.MAX_VALUE){
-                    // subtract 2 from  R.length to account for the sentinel value at the end of the array
-                    int positionOfItemToBeSwapped = mid + (R.length-2) + j;
-                    if(mid == 0){
+                    //If R.length is 2, then it contains 1 actual value from A and the sentinel value. Therefore, only subtract 1 from R.length to account for the sentinel
+                    // If R.length is greater than 2, then it contains more than 1 value from A and the sentinel value. Therefore, subtract 2 from  R.length to account for the sentinel value at the end of the array
+                    int positionOfItemToBeSwapped = mid + (R.length == 2 ? R.length-1 : R.length-2) + j;
+                    /*if(mid == 0){
                         positionOfItemToBeSwapped++;
-                    }
+                    }*/
                     inversions += positionOfItemToBeSwapped - k /*mid + (R.length-1) + j*/ /*end-k*/ /*inversions++*/;
                 }
                 j++;
@@ -51,11 +52,11 @@ public class MergeSort {
     }
 
 
-    public static int[] MergeSort(int[] A, int start, int end){
+    public static long[] MergeSort(long[] A, int start, int end){
         if(start < end){
             int mid = Math.floorDiv((start + end), 2);
             //Actually split the sequence A in half and pass a local copy of it B to the recursion
-            /*int[] B = new int[mid+1];
+            /*long[] B = new int[mid+1];
             for(int i = start; i <= mid; i++){
                 B[i] = A[i];
             }*/
@@ -66,14 +67,14 @@ public class MergeSort {
         return A ;
     }
 
-    public static void main(int[] sequence/*String[] args*/){
+    public static void main(long[] sequence/*String[] args*/){
         //Scanner scanner = new Scanner(System.in);
         //System.out.print("Enter the number of integers in the sequence: ");
-        //int[] sequence = new int[scanner.nextInt()];
-        //int[] sequence = new int[args[1].length()];
+        //long[] sequence = new int[scanner.nextInt()];
+        //long[] sequence = new int[args[1].length()];
 
        /* char[] charSequence = args[1].toCharArray();
-        int[]  sequence = new int[charSequence.length];
+        long[]  sequence = new int[charSequence.length];
         //System.out.println("Enter the sequence one integer at a time with a single space between consecutive characters: \n");
         for(int i = 0; i < sequence.length; i++){
             //sequence[i] = scanner.nextInt();
@@ -82,10 +83,10 @@ public class MergeSort {
 
         }*/
 
-        int[] sortedArray = MergeSort(sequence, 0, sequence.length-1);
+        long[] sortedArray = MergeSort(sequence, 0, sequence.length-1);
 
         System.out.println("The sorted sequence is: " );
-        for(int i: sortedArray) {
+        for(long i: sortedArray) {
             System.out.println(i);
         }
 
